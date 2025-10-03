17b65767326d67973116feec89565f3877e6f03c
WiillSnow <wm.baquedano@hotmail.com>
2025-10-03 13:02:52 -0600

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,11 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: "*", allow: "/" }
+      { userAgent: "*", allow: "/" },
       { userAgent: "*", disallow: ["/gracias"] },
     ],
     sitemap: "https://icarosolar.mx/sitemap.xml",
     host: "https://icarosolar.mx",
   };
 }
+
